2546306528935049fac6a82c860b5a62ad9a2688
Vamsi Kalagaturu <vamsikalagaturu@gmail.com>
2024-01-21 20:18:44 +0100

    added pid controller library

---
+++ include/controllers/pid_controller.hpp
@@ -0,0 +1,168 @@
+/**
+ * Author: Vamsi Kalagaturu
+ * Contributors: Ravisankar Selvaraju, Wing Ki Lau
+ * 
+ * Description: Library implmenting PID controller for the arm_actions package
+ *
+ * Copyright (c) [2023]
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+#ifndef PID_CONTROLLER_HPP
+#define PID_CONTROLLER_HPP
+
+#include <array>
+#include <tuple>
+#include <vector>
+#include <iostream>
+
+#include "kdl/frames.hpp"
+#include "kdl/jntarray.hpp"
+
+/**
+ * @brief PID Controller class for computing control signals.
+ */
+class PIDController
+{
+public:
+  /**
+   * @brief Constructs a PIDController object with the specified gains.
+   *
+   * @param Kp The proportional gain.
+   * @param Ki The integral gain.
+   * @param Kd The derivative gain.
+   * @param dt The time step or time difference.
+   */
+  PIDController(double Kp, double Ki, double Kd, double dt);
+
+  /**
+   * @brief Constructs a PIDController object with the specified gains.
+   *
+   * @param Kp The proportional gain.
+   * @param Ki The integral gain.
+   * @param Kd The derivative gain.
+   * @param dt The time step or time difference.
+   * @param threshold The threshold for the error.
+   * @param operator The operator to use for the threshold.
+   */
+  PIDController(double Kp, double Ki, double Kd, double dt, double threshold, std::string op);
+
+  /**
+   * @brief Constructs a PIDController object with the specified gains.
+   *
+   * @param Kp The proportional gain.
+   * @param Ki The integral gain.
+   * @param Kd The derivative gain.
+   * @param dt The time step or time difference.
+   * @param threshold The threshold for the error.
+   * @param operator The operator to use for the threshold.
+   */
+  PIDController(double Kp, double Ki, double Kd, double dt, KDL::Vector threshold, std::string op);
+
+  /**
+   * @brief Computes the control signal based on the current and target values.
+   *
+   * @param current_value The current values of the system (x, y, z).
+   * @param target_value The desired target values (x, y, z).
+   * @return The computed control signal as a tuple (u_x, u_y, u_z).
+   */
+  std::vector<double> computeControlSignal_3d(const std::array<double, 3>& current_value,
+                                              const std::array<double, 3>& target_value);
+
+  /**
+   * @brief Computes the control signal based on the current and target values.
+   * @param current_value The current values of the system kdl vector.
+   * @param target_value The desired target values kdl vector.
+   * @return The computed control signal as a kdl Vector.
+   */
+  KDL::Vector computeControlSignal_3d(const KDL::Vector& current_value,
+                                        const KDL::Vector& target_value);
+
+  /**
+   * @brief Computes the control signal based on the current and target values.
+   * @param current_value The current values of the system kdl vector.
+   * @param target_value The desired target values kdl vector.
+   * @return The computed control signal as a double value
+   */
+  double computeControlSignal_1d(const KDL::Vector& current_value,
+                                 const KDL::Vector& target_value);
+
+  /**
+   * @brief Computes the control signal based on the current and target values.
+   * @param current_value a single value (double)
+   * @param target_value a single value (double)
+   * @return The computed control signal as a double value
+   */
+  double computeControlSignal_1d(const double& current_value, const double& target_value);
+
+private:
+  /**
+   * @brief Calculates the error between two points.
+   *
+   * @param p1 The first point (x1, y1, z1).
+   * @param p2 The second point (x2, y2, z2).
+   * @return The error as a tuple (dx, dy, dz).
+   */
+  std::tuple<double, double, double> calc_error(const std::array<double, 3>& p1,
+                                                const std::array<double, 3>& p2);
+
+  /**
+   * @brief Calculates the error between two vectors.
+   *
+   * @param v1 The first vector.
+   * @param v2 The second vector.
+   * @return The error as a kdl vector.
+   */
+  KDL::Vector calc_error(const KDL::Vector& v1, const KDL::Vector& v2);
+
+  /**
+   * @brief Calculates the error between two vectors.
+   *
+   * @param v1 The first double.
+   * @param v2 The second double.
+   * @return The error as a doble.
+   */
+  double calc_error(const double& v1, const double& v2);
+
+  double Kp;  // Proportional gain
+  double Ki;  // Integral gain
+  double Kd;  // Derivative gain
+  double dt;  // Time step
+
+  std::string op;   // Operator to use for the threshold
+  double threshold = 0.0; // Threshold for the error
+
+  KDL::Vector threshoold_vec; // Threshold for the error
+
+  double error_sum_x;   // Accumulated error in the x-axis
+  double error_sum_y;   // Accumulated error in the y-axis
+  double error_sum_z;   // Accumulated error in the z-axis
+  double last_error_x;  // Previous error in the x-axis
+  double last_error_y;  // Previous error in the y-axis
+  double last_error_z;  // Previous error in the z-axis
+
+  double error_sum_1d;   // Accumulated error in 1 axis
+  double last_error_1d;  // Previous error in 1 axis
+
+  KDL::Vector error_sum;   // Accumulated error
+  KDL::Vector last_error;  // Previous error
+};
+
+#endif /* PID_CONTROLLER_HPP */
---
+++ src/pid_controller.cpp
@@ -0,0 +1,227 @@
+/**
+ * Author: Vamsi Kalagaturu
+ * Contributors: Ravisankar Selvaraju, Wing Ki Lau
+ * 
+ * Description: Library implmenting PID controller for the arm_actions package
+ *
+ * Copyright (c) [2023]
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+#include "controllers/pid_controller.hpp"
+
+PIDController::PIDController(double Kp, double Ki, double Kd, double dt)
+    : Kp(Kp),
+      Ki(Ki),
+      Kd(Kd),
+      dt(dt),
+      error_sum_x(0),
+      error_sum_y(0),
+      error_sum_z(0),
+      last_error_x(0),
+      last_error_y(0),
+      last_error_z(0),
+      error_sum_1d(0),
+      last_error_1d(0)
+{
+}
+
+PIDController::PIDController(double Kp, double Ki, double Kd, double dt, double threshold,
+                             std::string op)
+    : Kp(Kp),
+      Ki(Ki),
+      Kd(Kd),
+      dt(dt),
+      error_sum_x(0),
+      error_sum_y(0),
+      error_sum_z(0),
+      last_error_x(0),
+      last_error_y(0),
+      last_error_z(0),
+      error_sum_1d(0),
+      last_error_1d(0),
+      threshold(threshold),
+      op(op)
+{
+}
+
+PIDController::PIDController(double Kp, double Ki, double Kd, double dt, KDL::Vector threshold,
+                             std::string op)
+    : Kp(Kp),
+      Ki(Ki),
+      Kd(Kd),
+      dt(dt),
+      error_sum_x(0),
+      error_sum_y(0),
+      error_sum_z(0),
+      last_error_x(0),
+      last_error_y(0),
+      last_error_z(0),
+      error_sum_1d(0),
+      last_error_1d(0),
+      op(op)
+{
+  threshoold_vec = threshold;
+}
+
+std::vector<double> PIDController::computeControlSignal_3d(
+    const std::array<double, 3>& current_value, const std::array<double, 3>& target_value)
+{
+  auto [error_x, error_y, error_z] = calc_error(current_value, target_value);
+
+  // Proportional terms
+  std::tuple<double, double, double> proportional_term =
+      std::make_tuple(Kp * error_x, Kp * error_y, Kp * error_z);
+
+  // Integral terms
+  error_sum_x += error_x * dt;
+  error_sum_y += error_y * dt;
+  error_sum_z += error_z * dt;
+  std::tuple<double, double, double> integral_term =
+      std::make_tuple(Ki * error_sum_x, Ki * error_sum_y, Ki * error_sum_z);
+
+  // Derivative terms
+  std::tuple<double, double, double> derivative_term =
+      std::make_tuple(Kd * (error_x - last_error_x) / dt, Kd * (error_y - last_error_y) / dt,
+                      Kd * (error_z - last_error_z) / dt);
+
+  last_error_x = error_x;
+  last_error_y = error_y;
+  last_error_z = error_z;
+
+  std::vector<double> control_signal = {
+      std::get<0>(proportional_term) + std::get<0>(integral_term) + std::get<0>(derivative_term),
+      std::get<1>(proportional_term) + std::get<1>(integral_term) + std::get<1>(derivative_term),
+      std::get<2>(proportional_term) + std::get<2>(integral_term) + std::get<2>(derivative_term)};
+
+  return control_signal;
+}
+
+KDL::Vector PIDController::computeControlSignal_3d(const KDL::Vector& current_value,
+                                                     const KDL::Vector& target_value)
+{
+  KDL::Vector error = calc_error(current_value, target_value);
+
+  // Proportional terms
+  KDL::Vector proportional_term = error * Kp;
+
+  // Integral terms
+  error_sum += error * dt;
+  KDL::Vector integral_term = error_sum * Ki;
+
+  // Derivative terms
+  KDL::Vector derivative_term = (error - last_error) * Kd / dt;
+
+  last_error = error;
+
+  KDL::Vector control_signal;
+
+  for (int i = 0; i < 3; i++)
+  {
+    control_signal(i) = proportional_term(i) + integral_term(i) + derivative_term(i);
+  }
+
+  // check if any of target val is inf and if so, set the control signal to 0
+  for (int i = 0; i < 3; i++)
+  {
+    if (std::isinf(target_value(i)))
+    {
+      control_signal(i) = 0;
+    }
+  }
+
+  return control_signal;
+}
+
+double PIDController::computeControlSignal_1d(const KDL::Vector& current_value,
+                                              const KDL::Vector& target_value)
+{
+  KDL::Vector error = calc_error(current_value, target_value);
+
+  // Proportional terms
+  KDL::Vector proportional_term = error * Kp;
+
+  // Integral terms
+  error_sum += error * dt;
+  KDL::Vector integral_term = error_sum * Ki;
+
+  // Derivative terms
+  KDL::Vector derivative_term = (error - last_error) * Kd / dt;
+
+  last_error = error;
+
+  KDL::JntArray control_signal(6);
+
+  for (int i = 0; i < 3; i++)
+  {
+    control_signal(i) = proportional_term(i) + integral_term(i) + derivative_term(i);
+  }
+
+  // add all the values of the control signal and return the result
+  double sum = 0;
+  for (int i = 0; i < 3; i++)
+  {
+    sum += control_signal(i);
+  }
+
+  return sum;
+}
+
+double PIDController::computeControlSignal_1d(const double& current_value,
+                                              const double& target_value)
+{
+  double error = calc_error(current_value, target_value);
+
+  // Proportional terms
+  double proportional_term = error * Kp;
+
+  // Integral terms
+  error_sum_1d += error * dt;
+  double integral_term = error_sum_1d * Ki;
+
+  // Derivative terms
+  double derivative_term = (error - last_error_1d) * Kd / dt;
+
+  last_error_1d = error;
+
+  double control_signal = proportional_term + integral_term + derivative_term;
+
+  return control_signal;
+}
+
+std::tuple<double, double, double> PIDController::calc_error(const std::array<double, 3>& p1,
+                                                             const std::array<double, 3>& p2)
+{
+  double dx = p2[0] - p1[0];
+  double dy = p2[1] - p1[1];
+  double dz = p2[2] - p1[2];
+
+  return std::make_tuple(dx, dy, dz);
+}
+
+KDL::Vector PIDController::calc_error(const KDL::Vector& v1, const KDL::Vector& v2)
+{
+  return v2 - v1 - threshoold_vec;
+}
+
+double PIDController::calc_error(const double& v1, const double& v2)
+{
+  return v2 - v1 - threshold;
+}
